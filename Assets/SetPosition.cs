7d82989d9d05c78fed942cd5c705c67a4ccdfea7
Ferhat Eren Dalçık <f.dalcik@azerion.com>
2022-12-26 12:17:19 +0300

    texture dissolve update

--- Assets/SetPosition.cs
+++ Assets/SetPosition.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SetPosition : MonoBehaviour
 {
+    [SerializeField] private GameObject obj;
+    private Material _mat;
+    private void Start()
+    {
+        _mat = obj.GetComponent<Renderer>().sharedMaterial;
+    }
+
     // Update is called once per frame
     void Update()
     {
+       _mat.SetFloat("_Radius",Mathf.Lerp(_mat.GetFloat("_Radius"),10,0.2F * Time.deltaTime));  
         Shader.SetGlobalVector("_Position", transform.position);
+        
     }
 }
